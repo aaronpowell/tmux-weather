2ab2d31e43ef2fee052429e82377673f78f3f913
Latiif <latiif@mail.com>
2022-05-14 11:32:41 +0200

    Add option to specify location
    The option to specify location can be set with:
    `set -g @forecast-location "[VALUE]"`
    
    The contents of that field are passed as-is to `wttr.in`.
    By default, option is empty letting `wttr.in` figure out the location.

--- scripts/forecast.sh
+++ scripts/forecast.sh
@@ -6,8 +6,9 @@ source "$CURRENT_DIR/helpers.sh"
 
 print_forecast() {
   local format=$(get_tmux_option @forecast-format "%C+%t+%w")
+  local location=$(get_tmux_option @forecast-location "") # Let wttr.in figure out the location
   local char_limit=$(get_tmux_option @forecast-char-limit 75)
-  local forecast=$(curl http://wttr.in/?format=$format)
+  local forecast=$(curl http://wttr.in/$location?format=$format)
   echo ${forecast:0:$char_limit}
 }
 
